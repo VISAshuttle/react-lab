375f0c633f3821d087b445b330919a54f6f89a07
VISAshuttle <codemonkey@kakao.com>
2024-10-13 22:46:09 +0900

    구버전 react-router-dom 대응 코드 및 주석 추가

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Routes>
+        {/* react-router-dom 5버전까지 사용되는 코드
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route> */}
         <Route path="/movie/:id" element={<Detail />}></Route>
         <Route path="/" element={<Home />}></Route>
       </Routes>
